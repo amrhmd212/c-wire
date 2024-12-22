ba0401e0b97a26d4f051273d94181f1e458038fa
amrhmd212 <amirelbayed@gmail.com>
2024-12-22 01:56:09 +0100

    Mise à jour de verif.c
    Identation et commentaire du code

--- verif.c
+++ verif.c
@@ -1,18 +1,18 @@
 
 #include "verif.h"
 
-// Gestion des erreurs personnalisée
+// Gestion des erreurs.
 void gestion_erreur(const char* fonction, const char* cause) {
-    fprintf(stderr, "Erreur dans la fonction '%s' : %s\n", fonction, cause);
-    exit(EXIT_FAILURE);
+    fprintf(stderr, "Erreur dans la fonction '%s' : %s\n", fonction, cause);//message d erreur avec 2%s afin de personnaliser le message d erreur er fonction d'où le problème vient.
+    exit(EXIT_FAILURE);//sortie du programme.
 }
 
 // Libérer l'AVL
 void liberer_AVL(AVL* a) {
-    if (a == NULL) {
-        return;
+    if (a == NULL) {//verifiction si l'avl existe.
+        return;//retourn de la fonction.
     }
-    liberer_AVL(a->fg);
-    liberer_AVL(a->fd);
-    free(a);
+    liberer_AVL(a->fg);//appel de la fonction liberer_AVL pour liberer le fils gauche.
+    liberer_AVL(a->fd);//appel de la fonction liberer_AVL pour liberer le fils droit.
+    free(a);//libération de la mémoire.
 }
